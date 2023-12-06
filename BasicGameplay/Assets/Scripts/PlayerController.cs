2e57e1a3e0e2fdddf058bfafa9d50c06c929dbdd
Tanishi <patelt@roverkids.org>
2023-12-06 14:09:39 -0500

    2.2 incomplete

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+
+    public GameObject prrojectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +29,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
