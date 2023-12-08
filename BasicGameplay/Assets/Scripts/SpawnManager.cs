230959f1173133cffac62c8655eee1f268e6da8f
Tanishi <patelt@roverkids.org>
2023-12-08 13:45:12 -0500

    lesson 2.3 complete

--- BasicGameplay/Assets/Scripts/SpawnManager.cs
+++ BasicGameplay/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +17,9 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            int animalIndex = Random.Range(0, 3);
+
+            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-20, 20), 0, 20), animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
